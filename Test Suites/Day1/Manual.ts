9ac6d32ff5013487c3e1b720e6b6fa7bbff082e9
Muhammad Ikhsan Kurniawan <muhammadikhsank014@gmail.com>
2024-05-15 13:56:07 +0700

    new new

--- Test Suites/Day1/Manual.ts
+++ Test Suites/Day1/Manual.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Manual-Record-Script</name>
+   <name>Manual</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -23,12 +23,5 @@
          <value></value>
          <variableId>dde115d4-c0c6-4fff-bcbd-f60eb532719a</variableId>
       </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4be0a5f5-c115-439f-ab57-16c261b2fe81</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Day1/RecordModeDemo</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
